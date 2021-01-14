7d6c1a8b801b9389a862aeb2c386980857c17df6
jhondev-code <jhondev.code@gmail.com>
2021-01-13 22:27:46 -0500

    Avance del 13-01-2021

---
+++ 1.hola-mundo/index.php
@@ -0,0 +1,9 @@
+<?php
+    // primer programa en php
+    # otro comentario en php
+    /*Este es comentario en bloques*/
+    print "Hola mundo con la funcion print";
+    echo "Primer hola mundo el el lenguaje de programación php";
+    echo "Hola", "Mundo";
+    echo "<h1>Hola mundo a todos</h1>";
+?>
